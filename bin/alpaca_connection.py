914fed4c03697ccef8b1551e0c8d89772f0849ca
Lela Andrews <lelavioletandrews@gmail.com>
2021-02-20 16:17:02 -0800

    initial commit

---
+++ bin/alpaca_connection.py
@@ -0,0 +1,7 @@
+class AlpacaPaperSocket(REST):
+    def __init__(self):
+        super().__init__(
+            key_id='AK9XV3T34L4PX07BDTAO',
+            secret_key='lzyXwrHfycq0Gk5IhG4GWP6GgJtF89EbHVDYcIz2',
+            base_url='https://paper-api.alpaca.markets'
+        )
