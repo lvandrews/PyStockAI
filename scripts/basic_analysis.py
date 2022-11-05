d6ff45f470b9336941d883878d115353ea47eaa1
lvandrews <lelavioletandrews@gmail.com>
2022-11-05 13:14:22 -0700

    first commit

---
+++ scripts/basic_analysis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# 2022-10-01
+
+#setup(
+#    name='basic_analysis.py',
+#    version='0.1.0',
+#    description='Add basic data analysis to stock data',
+#    long_description=readme,
+#    author='Lela Andrews',
+#    author_email='lelavioletandrews@gmail.com',
+#    url='https://github.com/lvandrews',
+#    license=license,
+#    packages=find_packages(exclude=('tests', 'docs'))
+#)
