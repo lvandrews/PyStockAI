42e5d7b8e0dc1b0f10d21727fffc054dba98b13e
lvandrews <lelavioletandrews@gmail.com>
2022-11-16 22:06:35 -0800

    working on yahoo apis

--- scripts/retrieve_stock_data.py
+++ scripts/retrieve_stock_data.py
@@ -18,13 +18,17 @@
 desctext='retrieve_stock_data.py: Obtain historical stock data and store for analysis.'
 vers='retrieve_stock_data.py v0.1'
 
-# Parse inputs or provide help
+# Import libraries, find date
 import argparse, sys, json, time, os, pprint
 import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
 from os.path import dirname, abspath
-#from yahoo_fin import stock_info as si
-#import yfinance as yf
+import yahoo_fin.stock_info as si
+import yfinance as yf
+import pandas as pd
+
+today=dt.date.today()
+year_ago=today - dt.timedelta(days=365)
 
 # Define date string, repo directories, credentials
 date_now_notime = time.strftime("%Y-%m-%d")
@@ -37,9 +41,11 @@ av_creds = os.path.join(scriptdir,"creds.json")
 # Initialize parser
 parser = argparse.ArgumentParser(description=desctext)
 parser.add_argument("-t", "--ticker", help="Ticker abbreviation (e.g. AMZN, required)", type=str, metavar="", required=True)
+parser.add_argument("-s", "--source", help="Select data source; default = y1; y1 = yahoo_fin, y2 = yFinance", choices=["av", "y1", "y2"], type=str, metavar="", default="y1")
+parser.add_argument("-b", "--begin_date", help="Beginning date for analysis set (e.g. 2021-04-20, default = one year ago from present date)", type=str, metavar='', default=year_ago)
+parser.add_argument("-a", "--all_time", help="Use all available data (supersedes -b)", action="store_true")
 parser.add_argument("-d", "--data_type", help="Data type to retrieve (daily, daily_adj (p), intraday or intraday_ext; default=intraday)", choices=["daily", "adj_daily", "intraday", "intraday_ext"], type=str, metavar="", default="intraday")
 parser.add_argument("-n", "--interval", help="Time interval between data points (intraday only; 1min, 5min, 15min, 30min, 60min); default=5min)", choices=["1min", "5min", "15min", "30min", "60min"], type=str, metavar="", default="5min")
-parser.add_argument("-s", "--source", help="Select data source; default=AlphaVantage", choices=["alphavantage", "yahoo"], type=str, metavar="", default="alphavantage")
 parser.add_argument("-v", "--version", help="show program version", action="version", version=vers)
 parser.add_argument("-V", "--verbose", help="increase output verbosity", action="store_true")
 
@@ -71,13 +77,17 @@ print("Ticker symbol:", ticker)
 print("Data type:", dtype)
 if dtype == "intraday":
     print("Time interval:", intvl)
-if source == "alphavantage":
+if source == "av":
     print("API source: AlphaVantage")
-elif source == "yahoo":
-    print("API source: Yahoo Finance")
-    
+elif source == "y1":
+    print("API source: Yahoo Finance (yahoo_fin)")
+elif source == "y2":
+    print("API source: Yahoo Finance (yFinance)")    
+   
 # Retrieve data function AlphaVantage
-if source == "alphavantage":
+if source == "av":
+    print("\AlphaVantage finance calls disabled at this time\n ----- EXITING -----\n")
+    quit()
     def save_dataset(symbol, time_window):
         credentials = json.load(open(av_creds, 'r'))
         api_key = credentials['av_api_key']
@@ -87,23 +97,29 @@ if source == "alphavantage":
         elif time_window == 'daily':
             data, meta_data = ts.get_daily(symbol, outputsize='full')
         elif time_window == 'adj_daily':
-            print("\nadj_daily data disabled due to paywall at AlphaVantage\n ----- EXITING -----\n")
-            quit()
-            #data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+            #print("\nadj_daily data disabled due to paywall at AlphaVantage\n ----- EXITING -----\n")
+            #quit()
+            data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
         elif time_window == 'intraday_ext':
-            print("\nintraday_ext data disabled\n ----- EXITING -----\n")
-            quit()
-        #    data, meta_data = ts.get_intraday_extended(symbol, interval='15min')
+            #print("\nintraday_ext data disabled\n ----- EXITING -----\n")
+            #quit()
+            data, meta_data = ts.get_intraday_extended(symbol, interval='15min')
 
         data.to_csv(ticker_data_filename)
         
     save_dataset(ticker, dtype)
 
+
 # Retrieve data function Yahoo -- NEED TO FIX AS ALPHAVANTAGE NOW CHARGING FOR DAILY DATA --
-if source == "yahoo":
-    print("\nYahoo finance calls disabled at this time\n ----- EXITING -----\n")
-    quit()
-#   def save_dataset(symbol, time_window):
+if source == "y1":
+    def save_dataset(symbol, time_window):
+   
+       # Load data from Yahoo Finance using specified range or all time
+    #if args.all_time:
+        df = si.get_data(ticker)
+
+    #elif args.begin_date:
+     #   df = si.get_data(ticker, start_date=args.begin_date, end_date=today)
 #        credentials = json.load(open(av_creds, 'r'))
 #        api_key = credentials['av_api_key']
 #        print(symbol, time_window)
@@ -117,7 +133,37 @@ if source == "yahoo":
 #        elif time_window == 'intraday_ext':
 #            data, meta_data = ts.get_time_series_intraday_extended(symbol, interval='15min')
 #
-#       data.to_csv(ticker_data_filename)
+        df.to_csv(ticker_data_filename)
 
-#    save_dataset(ticker, dtype)
+    save_dataset(ticker, dtype)
+
+
+# Retrieve data function Yahoo -- NEED TO FIX AS ALPHAVANTAGE NOW CHARGING FOR DAILY DATA --
+if source == "y2":
+    def save_dataset(symbol, time_window):
+
+       # Load data from Yahoo Finance using specified range or all time
+#        yf.download(ticker)
+        df = yf.Ticker(ticker)
+        df.history(period="max")
+    
+
+
+#        credentials = json.load(open(av_creds, 'r'))
+#        api_key = credentials['av_api_key']
+#        print(symbol, time_window)
+#        ts = TimeSeries(key=api_key, output_format='pandas')
+#        if time_window == 'intraday':
+#            data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='full')
+#        elif time_window == 'daily':
+#            data, meta_data = ts.get_daily(symbol, outputsize='full')
+#        elif time_window == 'adj_daily':
+#            data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+#        elif time_window == 'intraday_ext':
+#            data, meta_data = ts.get_time_series_intraday_extended(symbol, interval='15min')
+#
+        d.to_csv(ticker_data_filename)
+
+    save_dataset(ticker, dtype)
+    
     
